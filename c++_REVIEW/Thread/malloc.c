fbbf58b9d81511586f7bf9163ce0499c6e8baff0
Alphalikegithub <2908926129@qq.com>
2024-05-25 08:32:32 +0800

    莫名其妙的bug

--- c++_REVIEW/Thread/malloc.c
+++ c++_REVIEW/Thread/malloc.c
@@ -1,21 +1,28 @@
-#include <stdlib.h>  
+#include <stdio.h>
+#include <stdlib.h>    
   
-int main() {  
-    int *ptr = (int*)malloc(sizeof(int));  
+    
+int main() {    
+    int *ptr = (int*)malloc(sizeof(int));    
+    if (ptr == NULL) {   
+        printf("Memory not allocated.\n");    
+        exit(0);    
+    }    
+    
+    *ptr = 42; // assign a value to the allocated memory location    
+    
+    printf("Value of *ptr: %d\n", *ptr);    
+    
+    free(ptr); // this releases the memory    
+    ptr = NULL; // set the pointer to NULL immediately after freeing  
   
+    // Checking if ptr is NULL before dereferencing it  
     if (ptr == NULL) {  
-        printf("Memory not allocated.\n");  
-        exit(0);  
+        printf("Pointer is NULL, cannot access *ptr.\n");  
+    } else {  
+        // This block will never execute because ptr is set to NULL above  
+        printf("Value of *ptr after free: %d\n", *ptr);  
     }  
-  
-    *ptr = 42; // assign a value to the memory location  
-  
-    printf("Value of *ptr: %d\n", *ptr);  
-  
-    free(ptr); // this releases the memory  
-  
-    // after freeing memory, you should not use ptr any more  
-    printf("Value of *ptr after free: %d\n", *ptr); // this is undefined behavior  
-  
-    return 0;  
+    
+    return 0;    
 }
